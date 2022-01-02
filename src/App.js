f43bbeb9bb428b9e055ce7a5ae248f65d12f94d5
heyameen <aladeameen@gmail.com>
2022-01-02 14:48:35 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <p>
+          Let's Get started
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
